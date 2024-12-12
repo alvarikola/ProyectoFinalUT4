1992cd09b396cba7ef8cf181eddb9e7ea29770b0
alvarikola <alvaroe484@gmail.com>
2024-12-12 12:16:37 +0000

    creada tareaDao

---
+++ app/src/main/java/com/example/proyectofinalut4/data/TareaDao.kt
@@ -0,0 +1,24 @@
+package com.example.proyectofinalut4.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TareaDao {
+
+    @Insert
+    suspend fun insertTarea(tarea: Tarea)
+
+    @Update
+    suspend fun update(tarea: Tarea)
+
+    @Delete
+    suspend fun delete(tarea: Tarea)
+
+
+    @Query("SELECT * FROM Tareas")
+    suspend fun getAllTareasAndTipos(): List<TareasWithTipo>
+}
