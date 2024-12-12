3932e35d2d4486f597efbb69b4842ae7590b93f6
alvarikola <alvaroe484@gmail.com>
2024-12-12 12:18:44 +0000

    creada relacion tareaWithPrioridad

---
+++ app/src/main/java/com/example/proyectofinalut4/data/TareaWithPrioridad.kt
@@ -0,0 +1,13 @@
+package com.example.proyectofinalut4.data
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class TareasWithPrioridad(
+    @Embedded val tarea: Tarea,
+    @Relation(
+        parentColumn = "idPrioridadOwner",
+        entityColumn = "idPrioridad"
+    )
+    val prioridadTareas: List<Prioridad>
+)
