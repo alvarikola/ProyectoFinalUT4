1c6efa20190835bcb17c1b4ffb73ae4aaaf83b15
alvarikola <alvaroe484@gmail.com>
2024-12-12 12:16:50 +0000

    creada tipoTareaDao

---
+++ app/src/main/java/com/example/proyectofinalut4/data/TipoTareaDao.kt
@@ -0,0 +1,24 @@
+package com.example.proyectofinalut4.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TipoTareaDao {
+
+    @Insert
+    suspend fun insertTipoTarea(tipoTarea: TipoTarea)
+
+    @Update
+    suspend fun update(tipoTarea: TipoTarea)
+
+    @Delete
+    suspend fun delete(tipoTarea: TipoTarea)
+
+    @Query("SELECT * FROM TiposTareas")
+    suspend fun getAllTipos(): List<TipoTarea>
+
+}
