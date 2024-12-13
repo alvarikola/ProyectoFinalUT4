ea3ccdb4c78189bd08ca6d13a0715cde694567f9
alvarikola <alvaroe484@gmail.com>
2024-12-13 13:58:04 +0000

    realizado main

--- app/src/main/java/com/example/proyectofinalut4/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinalut4/MainActivity.kt
@@ -7,33 +7,36 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.proyectofinalut4.data.AppDatabase
 import com.example.proyectofinalut4.ui.theme.ProyectoFinalUT4Theme
+import com.example.proyectofinalut4.view.TareaApp
+import com.example.proyectofinalut4.viewModel.MyViewModel
+import com.example.proyectofinalut4.viewModel.ViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val database = AppDatabase.getDatabase(this)
+        val tareaDao = database.tareaDao()
+        val tipoTareaDao = database.tipoTareaDao()
+
         enableEdgeToEdge()
         setContent {
             ProyectoFinalUT4Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                    val myViewModel: MyViewModel = viewModel(factory = ViewModelFactory(tareaDao, tipoTareaDao))
+
+
+                    TareaApp(
+                        modifier = Modifier.padding(innerPadding),
+                        myViewModel = myViewModel
                     )
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
