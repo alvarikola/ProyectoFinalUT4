13ff47f2c86ec73abe1a177101bdbbebac0de1b4
alvarikola <alvaroe484@gmail.com>
2024-12-13 11:32:00 +0000

    realizado viewModelFactory

--- app/src/main/java/com/example/proyectofinalut4/viewModel/ViewModelFactory.kt
+++ app/src/main/java/com/example/proyectofinalut4/viewModel/ViewModelFactory.kt
@@ -1,4 +1,15 @@
 package com.example.proyectofinalut4.viewModel
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.proyectofinalut4.data.TareaDao
+import com.example.proyectofinalut4.data.TipoTareaDao
+
+class ViewModelFactory(private val tareaDao: TareaDao, private val tipoTareaDao: TipoTareaDao) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(ViewModel::class.java)) {
+            return ViewModel(tareaDao, tipoTareaDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
