2d2a96907740c8f49166e98b92288d7c76a7dcac
alvarikola <alvaroe484@gmail.com>
2024-12-16 13:58:55 +0000

    pasado prioridadDao al viewModel

--- app/src/main/java/com/example/proyectofinalut4/viewModel/ViewModelFactory.kt
+++ app/src/main/java/com/example/proyectofinalut4/viewModel/ViewModelFactory.kt
@@ -2,13 +2,14 @@ package com.example.proyectofinalut4.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.proyectofinalut4.data.PrioridadDao
 import com.example.proyectofinalut4.data.TareaDao
 import com.example.proyectofinalut4.data.TipoTareaDao
 
-class ViewModelFactory(private val tareaDao: TareaDao, private val tipoTareaDao: TipoTareaDao) : ViewModelProvider.Factory {
+class ViewModelFactory(private val tareaDao: TareaDao, private val tipoTareaDao: TipoTareaDao, private val prioridadDao: PrioridadDao) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MyViewModel::class.java)) {
-            return MyViewModel(tareaDao, tipoTareaDao) as T
+            return MyViewModel(tareaDao, tipoTareaDao, prioridadDao) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
